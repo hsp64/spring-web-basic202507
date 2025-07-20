741b9b162989382d619c31e2fa30399756816e94
hsp64 <pphs604@gmail.com>
2025-07-20 16:55:09 +0900

    피드 관리 서비스 계층 추가: FeedService 구현 및 컨트롤러, DTO, 레포지토리 리팩토링

--- src/main/java/com/spring/basic/chap5_5/controller/FeedController.java
+++ src/main/java/com/spring/basic/chap5_5/controller/FeedController.java
@@ -1,7 +1,11 @@
 package com.spring.basic.chap5_5.controller;
 
+import com.spring.basic.chap5_5.dto.request.FeedCreateRequest;
+import com.spring.basic.chap5_5.dto.response.FeedDetailResponse;
+import com.spring.basic.chap5_5.dto.response.FeedListResponse;
 import com.spring.basic.chap5_5.entity.Feed;
 import com.spring.basic.chap5_5.repository.FeedRepository;
+import com.spring.basic.chap5_5.service.FeedService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // 피드에 관련된 요청과 응답을 처리
@@ -20,7 +25,8 @@ import java.util.List;
 @RequiredArgsConstructor // final 필드만 파리미터로 받는 생성자
 public class FeedController {
 
-    private final FeedRepository feedRepository;
+//    private final FeedRepository feedRepository;
+    private final FeedService feedService;
 
 //    @Autowired
 //    public FeedController(FeedRepository feedRepository) {
@@ -29,20 +35,20 @@ public class FeedController {
 
     // 피드 전체 조회 요청
     @GetMapping
-    public ResponseEntity<List<Feed>> feedList() {
+    public ResponseEntity<?> feedList() {
         // 데이터창고에서 피드목록을 가져와서 클라이언트에게 응답
         // 응답할 때 클라이언트에게 피룡한데이터만 정제해서 저야 함.
-        List<Feed> feeds = feedRepository.getFeeds();
+        List<FeedListResponse> responses = feedService.listProcess();
 
-        return ResponseEntity.ok().body(feeds);
+        return ResponseEntity.ok().body(responses);
     }
 
     // 피드 등록 요청
     @PostMapping
-    public ResponseEntity<?> createFeed(@RequestBody Feed feed) {
+    public ResponseEntity<?> createFeed(@RequestBody FeedCreateRequest dto) {
 
-        // repository에게 저장을 위임
-        feedRepository.save(feed);
+        // service 에게 저장을 위임
+        feedService.createProcess(dto);
 
         return ResponseEntity.ok("피드 저장 성공!");
 
@@ -51,14 +57,24 @@ public class FeedController {
     // 피드 삭제 요청 /api/v5-5/feeds/{id} : DELETE
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteFeed(@PathVariable Long id) {
-        feedRepository.deleteById(id);
+        try {
+
+        feedService.removeProcess(id);
         return ResponseEntity.ok("삭제 성공!!");
+        } catch (IllegalArgumentException e) {
+            return  ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 
     // 피드 개별조회 요청 /api/v5-5/{id} : GET
     @GetMapping("/{id}")
     public ResponseEntity<?> findOne(@PathVariable Long id) {
-        Feed feed = feedRepository.findById(id);
-        return ResponseEntity.ok(feed);
+
+        // 피드 Id, 생성시간 제외
+        // 작성자명 (writer), 피드내용은 (feed_content), 조회수 (view) 응답
+        FeedDetailResponse response = feedService.findOneProcess(id);
+
+        return ResponseEntity.ok(response);
     }
 }
